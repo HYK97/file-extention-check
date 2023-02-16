b40a59c957dd0ac4be8834f9510e0ef305bab152
HYKim <ddh963963@naver.com>
2023-02-16 17:12:52 +0900

    first commit

---
+++ src/main/java/com/flow/fileextensioncheck/FileExtensionCheckApplication.java
@@ -0,0 +1,13 @@
+package com.flow.fileextensioncheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileExtensionCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileExtensionCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flow/fileextensioncheck/FileExtensionCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flow.fileextensioncheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileExtensionCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
